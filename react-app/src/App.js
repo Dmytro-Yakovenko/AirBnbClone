d5df8ac5022d73ae4f0c6be2efdd550078b1098e
Dmytro-Yakovenko <d.yakovenko1986@gmail.com>
2023-09-23 21:58:11 -0700

    HomePage

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import SignupFormPage from "./components/SignupFormPage";
+import SignupFormPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import HomePage from "./pages/HomePage";
 import { authenticate } from "./store/session";
 import Navigation from "./components/Navigation";
 
@@ -19,7 +20,10 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
-          <Route path="/login" >
+              <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="/login">
             <LoginPage />
           </Route>
           <Route path="/signup">
@@ -27,11 +31,11 @@ function App() {
           </Route>
 
           <Route >
-           <NotFoundPage/>
+            <NotFoundPage />
           </Route>
-
         </Switch>
       )}
+     
     </>
   );
 }
