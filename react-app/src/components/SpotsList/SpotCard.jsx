765df53ca733976284d43e360536637a5186ef68
Dmytro-Yakovenko <d.yakovenko1986@gmail.com>
2023-10-08 22:18:11 -0700

    style

--- react-app/src/components/SpotsList/SpotCard.jsx
+++ react-app/src/components/SpotsList/SpotCard.jsx
@@ -5,30 +5,50 @@ import "./SpotList.css";
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+
+
+
+
+
+// import required modules
+import { Navigation } from "swiper/modules";
+
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
- 
+
   textAlign: "center",
   color: theme.palette.text.secondary,
 }));
 
 const SpotCard = ({ id }) => {
   const spot = useSelector((state) => state.spots.spots[id]);
-console.log(spot,333333)
+  console.log(spot, 333333);
   return (
     <Item>
       <li className="spot-card-item">
-       
-        <img
-          className="spot-card-image"
-          src={spot.spot_image[0].spot_image_url}
-          alt={spot.title}
-        />
-<p>{spot.rating} </p>
- <p>{spot.city}, {spot.state} </p>
- <h2>{spot.title}</h2>
- <p> $ {spot.price}</p>
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+          {spot?.spot_image?.map((item) => (
+            <SwiperSlide key={item.id}>
+              <img
+                className="spot-card-image"
+                src={item.spot_image_url}
+                alt={spot.title}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+
+        <p>{spot.rating} </p>
+        <p>
+          {spot.city}, {spot.state}{" "}
+        </p>
+        <h2>{spot.title}</h2>
+        <p> $ {spot.price}</p>
 
         <NavLink to={`/spots/${id}`}>Show Details</NavLink>
       </li>
